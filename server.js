d0568f6313aef964952c7722766ab60a003d72d4
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-01-02 15:51:25 -0800

    testing comments

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(routes);
 
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.listen(PORT, function () {
     console.log('App listening on PORT ' + PORT);
 });
+
+// Testing deploy
