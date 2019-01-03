b39d6fc50a2a5652696412c80e9013a25eea6289
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-01-03 14:47:43 -0800

    adding css to page and fixed add burger button

--- config/orm.js
+++ config/orm.js
@@ -3,30 +3,30 @@ const mysql = require('mysql');
 
 const orm = {
     selectAll: function (table, cb) {
-        let query = 'SELECT * FROM ??';
+        var query = 'SELECT * FROM ??';
         connection.query(query, [table], function (err, res) {
             if (err) throw err;
             cb(res);
         });
     },
     insertOne: function (table, columnOne, columnTwo, valueOne, valueTwo, cb) {
-        let query = 'INSERT INTO ?? (??, ??) VALUES (?, ?)';
-        let inserts = [table, columnOne, columnTwo, valueOne, valueTwo];
-        let string = mysql.format(query, inserts);
+        var query = 'INSERT INTO ?? (??, ??) VALUES (?, ?)';
+        var inserts = [table, columnOne, columnTwo, valueOne, valueTwo];
+        var string = mysql.format(query, inserts);
         connection.query(string, function (err, res) {
             if (err) throw err;
             cb(res);
         });
     },
     updateOne: function (table, columnOne, valueOne, columnTwo, valueTwo, cb) {
-        let query = 'UPDATE ?? SET ?? = ? WHERE ?? = ?'
+        var query = 'UPDATE ?? SET ?? = ? WHERE ?? = ?'
         connection.query(query, [table, columnOne, valueOne, columnTwo, valueTwo], function (err, res) {
             if (err) throw err;
             cb(res);
         });
     },
     deleteOne: function (table, columnOne, valueOne, cb) {
-        let query = 'DELETE FROM ?? WHERE ?? = ?';
+        var query = 'DELETE FROM ?? WHERE ?? = ?';
         connection.query(query, [table, columnOne, valueOne], function (err, res) {
             if (err) throw err;
             cb(res);
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,38 +4,30 @@ const burger = require("../models/burger.js");
 
 router.get("/", function (req, res) {
     burger.selectAll(function (data) {
-        const hbsObject = { burgers: data };
+        var hbsObject = { burgers: data };
         res.render("index", hbsObject);
     });
 });
 
-router.get("/add-burger", function (req, res) {
-    res.render("add-burger");
-});
-
 router.post("/api/burgers", function (req, res) {
-    burger.insertOne(req.body.newBurger, function (result) {
-        res.json({ id: result.insertId });
+    burger.insertOne(req.body.burger_name, function (result) {
+        res.redirect("/");
+        // res.json({ id: result.insertId });
     });
 });
 
-router.put("/api/burgers/:id", function (req, res) {
-    const id = req.params.id;
-    let changedStatus;
-    if (req.body.devour === 'true') { changedStatus = true }
-    if (req.body.devour === 'false') { changedStatus = false }
+router.put("/:id", function (req, res) {
+    var id = req.params.id;
 
-    burger.updateOne(id, changedStatus, function (result) {
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+    console.log("id", id);
+
+    burger.updateOne(id, function () {
+        res.redirect("/");
     });
 });
 
 router.delete("/api/burgers/:id", function (req, res) {
-    const id = req.params.id;
+    var id = req.params.id;
     burger.deleteOne(id, function (result) {
         if (result.affectedRows == 0) {
             return res.status(404).end();
